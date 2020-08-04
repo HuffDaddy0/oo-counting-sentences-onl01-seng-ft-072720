8b92824f4280b50f955fd91934663a8cf07ab679
Tyler Huffman <ty.huff24@gmail.com>
2020-08-04 17:15:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def count_sentences
-    array = self.split(".","!")
+    array = self.split(".")
+    array.split("!")
     array.split("?")
     binding.pry
     array.delete_if do |index|
